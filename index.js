93fcc3fcb3ae615694d7f5ca3ca12d01b2592751
Rance Zawada <razawada@gmail.com>
2018-01-19 21:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeLastKitten(kittens) {
-  return kittens.slice(0, kittens.length-1);
+function removeLastKitten(name) {
+  return name.slice(0, name.length-1);
 }
