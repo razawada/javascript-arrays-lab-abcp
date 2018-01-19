442a573650b097ff06e0c51b3dc6a6772bac2633
Rance Zawada <razawada@gmail.com>
2018-01-19 21:17:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(array) {
+  var lessKittens = [...kittens];
+  lessKittens.pop();
+  return lessKittens;
+}
+
+function removeFirstKitten(array) {
+  var lessKittens = [...kittens];
+  lessKittens.shift();
+  return lessKittens;
+}
